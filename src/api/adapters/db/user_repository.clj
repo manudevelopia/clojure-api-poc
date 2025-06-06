6fee9e7d049ae835bda7abcdc57657cf6ad56d7f
Manuel García <manuel.garcia@zooplus.com>
2025-06-06 15:12:02 +0200

    format sql properly

--- src/api/adapters/db/user_repository.clj
+++ src/api/adapters/db/user_repository.clj
@@ -7,22 +7,22 @@
   (all [_ active]
     (log/info "Fetching all users")
     (db/execute! ["select id
-                      ,username
-                      ,email
-                      ,is_active
-                      ,created_at
-                      ,updated_at
+                         ,username
+                         ,email
+                         ,is_active
+                         ,created_at
+                         ,updated_at
                 from meeknu.users
                 where is_active = ?"
                   active]))
   (by-name [_ name]
     (log/info "Fetching user" name)
     (db/execute-one! ["select id
-                     ,username
-                     ,email
-                     ,is_active
-                     ,created_at
-                     ,updated_at
+                             ,username
+                             ,email
+                             ,is_active
+                             ,created_at
+                             ,updated_at
                from meeknu.users
                where username = ?"
                       name])))
